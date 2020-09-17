ad7da43ff7352d945fe0f9fdd15ab10faadf411e
imshota <imshotaseo@icloud.com>
2020-09-17 14:16:54 +0900

    view hp

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../Firebase';
 import Viewtitle from '../components/view_mangatitle';
+import { List } from '@material-ui/core';
+import ListItemLink from '../components/ListItemLink';
 
 export default function ComicPage() {
-  const [comicRef, setComicRef] = useState<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>();
+  const [comics, setComics] = useState<firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>[]>([]);
 
-  useEffect(
-    () => {
-      // firebaseからサーチする
-      db.collection("comics").get()
-      .then(function(querySnapshot) {
-        var maxtime = 0;
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            if (maxtime < doc.data().createdAt) {
-              maxtime = doc.data().createdAt;
-              setComicRef(db.collection("comics").doc(doc.id));
-            }
-        });
-      })
-      .catch(function(error) {
-        console.log("Error getting documents: ", error);
-      });
-      }, []
-  );
+  useEffect(() => {
+    const f = async () => {
+      try {
+        const querySnapshot = await db.collection("comics").orderBy("createdAt", "desc").get();
+        setComics(querySnapshot.docs);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    f();
+  }, [])
 
   return (
     <div className="ComicPage">
       <p>最新作!</p>
-      <Viewtitle comicRef={comicRef} />
+      <List>
+        {
+          comics.map((comic) => {
+            var d = new Date(comic.data().createdAt.toDate());
+            var year  = d.getFullYear();
+            var month = d.getMonth() + 1;
+            var day   = d.getDate();
+            var hour  = ( d.getHours()   < 10 ) ? '0' + d.getHours()   : d.getHours();
+            var min   = ( d.getMinutes() < 10 ) ? '0' + d.getMinutes() : d.getMinutes();
+            var sec   = ( d.getSeconds() < 10 ) ? '0' + d.getSeconds() : d.getSeconds();
+            return (
+              <ListItemLink 
+              key={comic.id} 
+              primary={comic.data().title + "   投稿日時 : " + year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec} 
+              to={`/comic/${comic.id}`} 
+              />
+            )
+          })
+        }
+      </List>
     </div>
   );
 }
