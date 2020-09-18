93edf7812740bed3947d53412abad9a8b9036859
tsuziken0917 <ihacoq@icloud.com>
2020-09-18 12:22:58 +0900

    0コメント=白　見出し訂正

--- src/pages/ComicPage.tsx
+++ src/pages/ComicPage.tsx
@@ -16,22 +16,42 @@ const useStyles = makeStyles(() =>
 
   createStyles({
     "comicPage": {
-      background:"#E9E9E9",
-      paddingTop:"50px",
+      background: "#E9E9E9",
+      paddingTop: "50px",
     },
-    "commentMainBox":{
-      position:"fixed",
-      top:"80px",
-      width:"30%"
+    "commentMainBox": {
+      position: "fixed",
+      top: "80px",
+      width: "30%"
     },
-    "commentBox":{
-      background:"rgb(250,250,250)",
-      padding:"20px 0",
-      borderRadius:"20px",
-      boxShadow:"0 0 30px grey", 
+    "commentBox": {
+      background: "rgb(250,250,250)",
+      padding: "20px 0",
+      borderRadius: "20px",
+      boxShadow: "0 0 30px grey",
     },
-    "commentTitle":{
+    "creatorPro":{
+      display: "flex",
+      justifyContent: "center",
+      background:"#364e96",
+      width:"85%",
+      margin:"0 auto",
+      boxShadow: "0 0 20px grey",
+      borderRadius: "10px",
+      fontWeight:"bold",
+      padding:"10px 0",
+      color:"#FFFFFF"
+    },
+    "creatorCard":{
       
+    },
+    "creatorToko":{
+      lineHeight: "48px", 
+      fontSize: "24px",
+
+    },
+    "commentTitle": {
+
     },
     "nagesenButton": {
       display: "flex",
@@ -39,20 +59,33 @@ const useStyles = makeStyles(() =>
     },
     "comicMainBox": {
       display: "flex",
-      justifyContent: "center",   
-      width:"70%",
-      marginLeft:"auto"
+      justifyContent: "center",
+      width: "70%",
+      marginLeft: "auto"
     },
     "comicBox": {
       display: "flex",
       justifyContent: "center",
+      flexDirection: "column",
       width: "85%",
-      background:"rgb(250,250,250)",
-      padding:"20px 0",
-      borderRadius:"20px",
-      boxShadow:"0 0 30px grey"
+      background: "rgb(250,250,250)",
+      padding: "20px 0",
+      borderRadius: "20px",
+      boxShadow: "0 0 30px grey",
+      textAlign:"center"
     },
-    "comic":{
+    "comicTitle": {
+      width:"85%",
+      margin:"40px auto",
+      background:"#364e96",
+      padding:"30px 0",
+      color:"#FFFFFF",
+      borderRadius: "10px",
+      fontWeight:"bold",
+      fontSize:"25px",
+      boxShadow: "0 0 20px grey",
+    },
+    "comic": {
 
     },
   }),
@@ -64,15 +97,17 @@ export default function ComicPage() {
   const { comicId } = useParams<{ comicId: string }>();
   const [comicRef, setComicRef] = useState<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>();
   const [comments, setComments] = useState<firebase.firestore.QueryDocumentSnapshot<firebase.firestore.DocumentData>[]>([]);
-  const [comicCreatorRef, setComicCreatorRef]=useState<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>();
+  const [comicCreatorRef, setComicCreatorRef] = useState<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>();
   const [title, setTitle] = useState<string>("");
+  let white:string="#FFFFFF";
+
 
   async function updateComments(comicRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>) {
-      const querySnapshot = await db.collection("comments")
-        .where("comicRef", "==", comicRef)
-        .orderBy("createdAt", "desc")
-        .get();
-      setComments(querySnapshot.docs);
+    const querySnapshot = await db.collection("comments")
+      .where("comicRef", "==", comicRef)
+      .orderBy("createdAt", "desc")
+      .get();
+    setComments(querySnapshot.docs);
   }
 
 
@@ -81,16 +116,16 @@ export default function ComicPage() {
 
     const comicRef = db.collection("comics").doc(comicId);
     comicRef.get()
-      .then(async function(doc) {
+      .then(async function (doc) {
         if (doc.exists) {
-            setComicCreatorRef(doc.data()!.userRef);
-            setTitle(doc.data()!.title);
+          setComicCreatorRef(doc.data()!.userRef);
+          setTitle(doc.data()!.title);
         } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
         }
-      }).catch(function(error) {
-          console.log("Error getting document:", error);
+      }).catch(function (error) {
+        console.log("Error getting document:", error);
       })
 
     setComicRef(comicRef);
@@ -99,28 +134,26 @@ export default function ComicPage() {
 
   return (
     <div className={useClasses.comicPage}>
-        <div className={useClasses.commentMainBox}>
-          <div className={useClasses.commentBox}>
-            <div style={{display: "flex", justifyContent: "center"}}>
-              <UserCard userRef={comicCreatorRef}></UserCard>
-              <span style={{lineHeight: "48px", fontSize: "24px"}}>(投稿者)</span>
-            </div>
-            <Comments comments={comments} comicRef={comicRef} />
-            
-            <div className={useClasses.nagesenButton}>
-              <NagesenButton comicRef={comicRef} updateComments={updateComments} />
-            </div>
+      <div className={useClasses.commentMainBox}>
+        <div className={useClasses.commentBox}>
+          <div className={useClasses.creatorPro}>
+            <UserCard userRef={comicCreatorRef} color={white}/>
+            <span className={useClasses.creatorToko} >(投稿者)</span>
           </div>
-        </div>
-      
-        <div className={useClasses.comicMainBox}>
-          <div className={useClasses.comicBox}>
-            <h3>{title}</h3>
-            <Header comicRef={comicRef}/>
-          </div>
-        </div>
-   
+          <Comments comments={comments} comicRef={comicRef} />
 
+          <div className={useClasses.nagesenButton}>
+            <NagesenButton comicRef={comicRef} updateComments={updateComments} />
+          </div>
+        </div>
+      </div>
+
+      <div className={useClasses.comicMainBox}>
+        <div className={useClasses.comicBox}>
+          <div className={useClasses.comicTitle}>{title}</div>
+          <Header comicRef={comicRef} />
+        </div>    
+      </div>
     </div>
   );
 }
